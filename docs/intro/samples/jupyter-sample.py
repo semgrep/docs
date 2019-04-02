eb1f141ac0f5bc64c78ed8d3d310beb4cb0400a5
Luke O'Malley <luke@returntocorp.com>
2019-04-01 18:55:32 -0700

    Python-ify minifinder analysis!

--- docs/intro/samples/jupyter-sample.py
+++ docs/intro/samples/jupyter-sample.py
@@ -2,9 +2,10 @@ import os
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
 
 ###################################
-# START OF EDITABLE CONSTANTS
+# EDIT THESE CONSTANTS
 ###################################
 
 GROUP = "YOUR-GROUP"
@@ -15,18 +16,18 @@ ANALYZER_VERSION = "0.1.0"
 CORPUS_NAME = "npm-1000-2019-01-01"
 
 ###################################
-# END OF EDITABLE CONSTANTS
+# END EDIT SECTION
 ###################################
 
-ANALYZER_SPECIFIC_QUERY = """
-SELECT result.commit_hash
-FROM  result,
-      commit_corpus
-WHERE corpus_name = %(corpus_name)s
-      AND analyzer_name = %(analyzer_name)s
-      AND analyzer_version = %(analyzer_version)s
-      AND commit_corpus.commit_hash = result.commit_hash
-GROUP BY result.commit_hash;
+# Canonical SQL query to get job-specific results back.
+JOB_QUERY = """
+SELECT * 
+FROM   result, 
+       commit_corpus 
+WHERE  result.commit_hash = commit_corpus.commit_hash 
+       AND analyzer_name = %(analyzer_name)s 
+       AND analyzer_version = %(analyzer_version)s
+       AND corpus_name = %(corpus_name)s
 """
 
 QUERY_PARAMS = {
@@ -35,6 +36,17 @@ QUERY_PARAMS = {
     "analyzer_version": ANALYZER_VERSION
 }
 
+# Connect to PostgreSQL host and query for job-specific results
 engine = create_engine(f'postgresql://notebook_user:{DB_PASSWORD}@{GROUP}-db.massive.ret2.co/postgres')
-analyzer_specific_df = pd.read_sql(analyzer_specific_query, engine, params=QUERY_PARAMS)
-analyzer_specific_df
+job_df = pd.read_sql(JOB_QUERY, engine, params=QUERY_PARAMS)
+
+# Print pandas dataframe to stdout for debugging
+print("Raw job dataframe:")
+print(job_df[1:10])
+
+# Add 'percent_whitespace' column, computed from the whitespace and total fields in our 'extra' column
+job_df['percent_whitespace'] = job_df.apply(lambda row: row['extra']['whitespace']/row['extra']['total'], axis=1)
+
+# Create a histogram of our data using the `percent_whitespace` column
+job_df.hist(column="percent_whitespace", bins=100)
+plt.show()
