33c8988ec1ad1e6ca4bf6e908e5bfde69830285d
Isaac Evans <ine@mit.edu>
2019-07-11 22:39:16 -0700

    build passes

--- docs/pipelines/samples/typed-ast-rules-example/src/analyze.sh
+++ docs/pipelines/samples/typed-ast-rules-example/src/analyze.sh
@@ -4,4 +4,4 @@ set -e
 AST_DIR="/analysis/inputs/r2c/typed-ast"
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-node $DIR/dist/src/index.js $AST_DIR >/analysis/output/output.json
+node $DIR/dist/index.js $AST_DIR >/analysis/output/output.json
