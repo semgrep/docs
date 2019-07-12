95686fb61b24e2d403c95d1928dd678cad6ab486
Isaac Evans <ine@mit.edu>
2019-07-12 11:48:24 -0700

    move typed-ast-rules-example to submodule

--- docs/pipelines/samples/typed-ast-rules-example/src/analyze.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-AST_DIR="/analysis/inputs/r2c/typed-ast"
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-node $DIR/dist/index.js $AST_DIR >/analysis/output/output.json
